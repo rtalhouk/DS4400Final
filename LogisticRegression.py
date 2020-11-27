fbc049c66aead6683d698e814bdc9842f88f632c
Conner Nilsen <nilsen.c@husky.neu.edu>
2020-11-27 11:30:00 -0800

    Update pickling for logistic regression and add trained pickle

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,20 +1,60 @@
+import json
+import pandas as pd
+import pickle
+import time
 from ImageLoader import ImageLoader
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
+from typing import Tuple
 
 
-def main() -> None:
-    lr = LogisticRegression(penalty="l1", solver="saga")
-    scaler = StandardScaler(copy=False)
+def load_image_dataset() -> Tuple[pd.DataFrame, pd.Series]:
     loader = ImageLoader()
     features, y_train = loader.load_images()
+    return features, y_train
+
+
+def scale_data(features: pd.DataFrame) -> pd.DataFrame:
+    start = time.time()
+    scaler = StandardScaler(copy=False)
     scaler.fit(features)
     x_train = scaler.transform(features)
+    print("Scaler finished in:", time.time() - start, "seconds")
+    return x_train
+
+
+def fit_log_reg(x_train: pd.DataFrame, y_train: pd.Series) -> LogisticRegression:
+    start = time.time()
+    lr = LogisticRegression(penalty="l1", solver="saga", tol=1e-2, max_iter=50)
     lr.fit(x_train, y_train)
-    print(lr.score(x_train, y_train))
-    return
+    print("Logistic regression fit finished in:", (time.time() - start) / 3600, "hours")
+    return lr
+
+
+def score_fit_log_reg(lr: LogisticRegression, x_data: pd.DataFrame, y_data: pd.Series) -> None:
+    start = time.time()
+    print(lr.score(x_data, y_data))
+    print("LR scoring finished in:", time.time() - start, "seconds")
+
+
+def save_theta(lr: LogisticRegression, filename: str = "log_reg_theta.pkl") -> None:
+    with open(filename, "wb") as file:
+        pickle.dump(lr, file)
+
+
+def load_theta(filename: str = "log_reg_theta.pkl") -> LogisticRegression:
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def train_lr_model() -> None:
+    features, target = load_image_dataset()
+    # TODO: Get train/test data
+    x_train = scale_data(features)
+    lr = fit_log_reg(x_train, target)
+    score_fit_log_reg(lr, x_train, target)
+    save_theta(lr)
 
 
 if __name__ == "__main__":
-    main()
+    train_lr_model()
---
+++ read_letter.py
@@ -0,0 +1,21 @@
+from LogisticRegression import load_image_dataset, load_theta
+from ImageLoader import ImageLoader
+from PIL import Image
+
+
+def main():
+    il = ImageLoader()
+    _, target = il.load_images()
+    features = il.scale_data()
+    test = ImageLoader(image_dir="./images/temp_test").load_images()[0]
+    scaled_test = il.scale_data(test)
+    lr = load_theta()
+    lt = ImageLoader("./images/asl_alphabet_test_2/asl-alphabet-test")
+    test_x, test_y = lt.load_images()
+    test_x_scaled = il.scale_data(test_x)
+    print(lr.predict(scaled_test))
+    print(lr.score(test_x_scaled, test_y))
+
+
+if __name__ == "__main__":
+    main()
