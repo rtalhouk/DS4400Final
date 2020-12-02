2373ce8a7e23b807e77683ff1a8fc4eb99c51fff
connernilsen@gmail.com <connernilsen@gmail.com>
2020-12-02 21:50:39 +0000

    asldkfj

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -17,12 +17,20 @@ def load_image_dataset() -> Tuple[pd.DataFrame, pd.Series]:
 
 def grid_search_log_reg(x_train: pd.DataFrame, y_train: pd.Series) -> LogisticRegression:
     start = time.time()
-    grid = GridSearchCV(LogisticRegression(penalty="l1", solver="saga", tol=1e-2, max_iter=50),
-                        param_grid={"C": [.5, .1, .05, .01]})
+    grid = GridSearchCV(LogisticRegression(penalty="l1", solver="saga", tol=1e-2, max_iter=70),
+                        param_grid={"C": [.5, .1, .01]}, cv=2)
     grid.fit(x_train, y_train)
     print("Logistic regression grid search fit finished in:", (time.time() - start) / 3600, "hours")
     print("Best Params:", grid.best_params_, "Best Score:", grid.best_score_)
     return grid.best_estimator_
+
+
+def create_best(x_train: pd.DataFrame, y_train: pd.Series) -> LogisticRegression:
+    start = time.time()
+    lr = LogisticRegression(penalty="l1", solver="saga", tol=1e-2, max_iter=70, C=.05)
+    lr.fit(x_train, y_train)
+    print("Logistic regression final fit finished in:", (time.time() - start) / 3600, "hours")
+    return lr
 
 
 def save_theta(lr: LogisticRegression, filename: str = "log_reg_theta.pkl") -> None:
@@ -37,7 +45,8 @@ def load_theta(filename: str = "log_reg_theta.pkl") -> LogisticRegression:
 
 def train_lr_model() -> None:
     features, target = load_image_dataset()
-    lr = grid_search_log_reg(features, target)
+    #lr = grid_search_log_reg(features, target)
+    lr = create_best(features, target)
     save_theta(lr)
 
 
