c6493d81f6d4f5edf734132bde1f2e7764f157ca
Conner Nilsen <nilsen.c@husky.neu.edu>
2020-12-09 09:14:17 -0800

    re-run lr

--- logistic_regression/LogisticRegression.py
+++ logistic_regression/LogisticRegression.py
@@ -9,11 +9,12 @@ from sklearn.model_selection import GridSearchCV
 def grid_search_log_reg(x_train: pd.DataFrame, y_train: pd.Series) -> LogisticRegression:
     start = time.time()
     grid = GridSearchCV(LogisticRegression(penalty="l1", solver="saga", tol=1e-2, max_iter=70),
-                        param_grid={"C": [.5, .1, .05, .01]}, cv=2, n_jobs=3)
+                        param_grid={"C": [1, .5, .1]}, cv=3, n_jobs=3)
     grid.fit(x_train, y_train)
     print("Logistic regression grid search fit finished in:", (time.time() - start) / 3600, "hours")
     print("Best Params:", grid.best_params_, "Best Score:", grid.best_score_)
     print(grid.cv_results_)
+    pd.DataFrame(data=grid.cv_results_).to_csv("lr_gs_res.csv")
     return grid.best_estimator_
 
 
